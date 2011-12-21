2ab7acb101df56043e6ede82b4f0294a9c789cb4
hkimura <hideaki.kimura@gmail.com>
2011-12-20 21:51:39 -0500

    Added Hive's antlr grammar

--- lvfs/src/java/edu/brown/lasvegas/qe/QueryPlan.java
+++ lvfs/src/java/edu/brown/lasvegas/qe/QueryPlan.java
@@ -2,6 +2,8 @@ package edu.brown.lasvegas.qe;
 
 /**
  * Represents a query plan.
+ * Unlike the syntax tree of SQL ({@link ParsedQuery}), this might be a DAG.
+ * However, we always have a single root node.
  */
 public class QueryPlan {
 
