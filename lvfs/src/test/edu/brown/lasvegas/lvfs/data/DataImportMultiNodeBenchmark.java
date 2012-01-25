a53b7f19e04072e2ebd7a2aa74f3a67423a915a8
hkimura <hideaki.kimura@gmail.com>
2012-01-25 15:22:35 -0500

    Multi-node data import benchmark

--- lvfs/src/test/edu/brown/lasvegas/lvfs/data/DataImportMultiNodeBenchmark.java
+++ lvfs/src/test/edu/brown/lasvegas/lvfs/data/DataImportMultiNodeBenchmark.java
@@ -1,6 +1,26 @@
 package edu.brown.lasvegas.lvfs.data;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.HashMap;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.log4j.Logger;
+
+import edu.brown.lasvegas.ColumnType;
+import edu.brown.lasvegas.LVColumn;
+import edu.brown.lasvegas.LVDatabase;
+import edu.brown.lasvegas.LVJob;
+import edu.brown.lasvegas.LVRack;
+import edu.brown.lasvegas.LVRackNode;
+import edu.brown.lasvegas.LVReplicaGroup;
+import edu.brown.lasvegas.LVTable;
+import edu.brown.lasvegas.LVTask;
+import edu.brown.lasvegas.client.LVMetadataClient;
+import edu.brown.lasvegas.protocol.LVMetadataProtocol;
+import edu.brown.lasvegas.server.LVDataNode;
 import edu.brown.lasvegas.server.StandaloneCentralNode;
+import edu.brown.lasvegas.util.ValueRange;
 
 /**
  * A performance benchmark program to test large data import
@@ -27,7 +47,8 @@ import edu.brown.lasvegas.server.StandaloneCentralNode;
  * <h2>Step 2</h2>
  * <p>
  * Configure an xml file. Examples are in src/test/edu/brown/lasvegas/server/lvfs_conf_xxx.xml.
- * At least modify the followings:
+ * At least modify the followings for your environment:
+ * lasvegas.server.meta.address,
  * lasvegas.server.data.address,
  * lasvegas.server.data.node_name,
  * lasvegas.server.data.rack_name
@@ -44,6 +65,8 @@ import edu.brown.lasvegas.server.StandaloneCentralNode;
  * <pre>
  * ant -Dconfxml=lvfs_conf_poseidon.xml sa-central-stop
  * </pre>
+ * Alternatively, you can of course put our jar and conf xml in hadoop's folder
+ * and launch Hadoop name node, but it's tedious.
  * </p>
  * 
  * <h2>Step 4</h2>
@@ -57,8 +80,141 @@ import edu.brown.lasvegas.server.StandaloneCentralNode;
  * <pre>
  * ant -Dconfxml=lvfs_conf_poseidon.xml -Daddress=poseidon:28712 sa-data-stop
  * </pre>
+ * Alternatively, you can of course put our jar and conf xml in hadoop's folder
+ * and launch Hadoop data node, but it's tedious.
+ * </p>
+ * 
+ * <h2>Step 5</h2>
+ * <p>
+ * Generate and place input files. You are gonna define its full path
+ * in this program (so far hard-coded, sorry!).
+ * </p>
+ * 
+ * <h2>Step 6</h2>
+ * <p>
+ * And then, finally launch this class.
  * </p>
  */
 public class DataImportMultiNodeBenchmark {
+    private static final Logger LOG = Logger.getLogger(DataImportMultiNodeBenchmark.class);
+    /** hardcoded total count of partitions. */
+    private static final int partitionCount = 12;
+    /** hardcoded full path of input files in ALL nodes (yes, I'm lazy. but this is just a benchmark..). It should be a partitioned tbl (see SSB/TPCH's dbgen manual). */
+    private static final String inputFilePath = "/home/hkimura/workspace/las-vegas/ssb-dbgen/lineorder_s12_p.tbl";
 
+    private Configuration conf;
+    private LVMetadataClient client;
+    private LVMetadataProtocol metaRepo;
+
+    private LVRack rack;
+    private LVRackNode[] nodes;
+    private LVDatabase database;
+    private LVTable table;
+    private HashMap<String, LVColumn> columns;
+    private int[] columnIds;
+    private LVReplicaGroup group;
+    
+    private void setUp () throws IOException {
+        conf = new Configuration();
+        client = new LVMetadataClient(conf);
+        LOG.info("connected to metadata repository");
+        metaRepo = client.getChannel();
+
+        String rackName = conf.get(LVDataNode.DATA_RACK_NAME_KEY);
+        rack = metaRepo.getRack(rackName);
+        if (rack == null) {
+            throw new IOException ("Rack not defined yet: " + rackName);
+        }
+        nodes = metaRepo.getAllRackNodes(rack.getRackId());
+        if (nodes.length == 0) {
+            throw new IOException ("No node defined in this rack: " + rackName);
+        }
+        
+        final String dbname = "db1";
+        if (metaRepo.getDatabase(dbname) != null) {
+            metaRepo.dropDatabase(metaRepo.getDatabase(dbname).getDatabaseId());
+            LOG.info("dropped existing database");
+        }
+
+        database = metaRepo.createNewDatabase(dbname);
+        final String[] columnNames = MiniLineorder.getColumnNames();
+        columns = new HashMap<String, LVColumn>();
+        table = metaRepo.createNewTable(database.getDatabaseId(), "lineorder", columnNames, MiniLineorder.getScheme());
+        for (LVColumn column : metaRepo.getAllColumnsExceptEpochColumn(table.getTableId())) {
+            columns.put(column.getName(), column);
+        }
+        columnIds = new int[columnNames.length];
+        for (int i = 0; i < columnIds.length; ++i) {
+            columnIds[i] = metaRepo.getColumnByName(table.getTableId(), columnNames[i]).getColumnId();
+        }
+        ValueRange[] ranges = new ValueRange[partitionCount];
+        for (int i = 0; i < partitionCount; ++i) {
+            ranges[i] = new ValueRange ();
+            ranges[i].setType(ColumnType.INTEGER);
+            if (i == 0) {
+                ranges[i].setStartKey(null);
+            } else {
+                ranges[i].setStartKey(6000000 * i + 1);
+            }
+            if (i == partitionCount - 1) {
+                ranges[i].setEndKey(null);
+            } else {
+                ranges[i].setEndKey(6000000 * (i + 1) + 1);
+            }
+        }
+        group = metaRepo.createNewReplicaGroup(table, columns.get("lo_orderkey"), ranges);
+        metaRepo.createNewReplicaScheme(group, columns.get("lo_orderkey"), columnIds, MiniLineorder.getDefaultCompressions());
+        metaRepo.createNewReplicaScheme(group, columns.get("lo_suppkey"), columnIds, MiniLineorder.getDefaultCompressions());
+        metaRepo.createNewReplicaScheme(group, columns.get("lo_orderdate"), columnIds, MiniLineorder.getDefaultCompressions());
+    }
+    private void tearDown () throws IOException {
+        if (client != null) {
+            client.release();
+            client = null;
+        }
+    }
+    public void exec () throws Exception {
+        ImportFractureJobParameters params = new ImportFractureJobParameters(table.getTableId());
+        for (LVRackNode node : nodes) {
+            params.getNodeFilePathMap().put(node.getNodeId(), new String[]{inputFilePath});
+        }
+        ImportFractureJobController controller = new ImportFractureJobController(metaRepo);
+        LOG.info("started the import job...");
+        LVJob job = controller.startSync(params);
+        LOG.info("finished the import job...:" + job);
+        for (LVTask task : metaRepo.getAllTasksByJob(job.getJobId())) {
+            LOG.info("Sub-Task finished in " + (task.getFinishedTime().getTime() - task.getStartedTime().getTime()) + "ms:" + task);
+        }
+    }
+    public static void main (String[] args) throws Exception {
+        LOG.info("running a multi node experiment..");
+        if (args.length == 0) {
+            System.err.println("usage: java " + DataImportMultiNodeBenchmark.class.getName() + " <conf xml path in classpath>");
+            System.err.println("ex: java -server -Xmx256m " + DataImportMultiNodeBenchmark.class.getName() + " lvfs_conf.xml");
+            return;
+        }
+        try {
+            InputStream test = StandaloneCentralNode.class.getResourceAsStream(args[0]);
+            test.read();
+            test.close();
+        } catch (Exception ex) {
+            System.err.println (args[0] + " cannot be read as a resource. Is it in the classpath?");
+            return;
+        }
+        Configuration.addDefaultResource(args[0]);
+        DataImportMultiNodeBenchmark program = new DataImportMultiNodeBenchmark();
+        program.setUp();
+        try {
+            LOG.info("started:" + inputFilePath + " (" + (program.nodes.length) + " nodes, " + partitionCount + " partitions)");
+            long start = System.currentTimeMillis();
+            program.exec();
+            long end = System.currentTimeMillis();
+            LOG.info("ended:" + inputFilePath + " (" + (program.nodes.length) + " nodes, " + partitionCount + " partitions)" + ". elapsed time=" + (end - start) + "ms");
+        } catch (Exception ex) {
+            LOG.error("unexpected exception:" + ex.getMessage(), ex);
+        } finally {
+            program.tearDown();
+        }
+        LOG.info("exit");
+    }
 }
